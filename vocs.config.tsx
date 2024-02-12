667b4f72b331c949df86e65d9dda3aeb0eaf9c21
Shivanshi <shivanshityagi@icloud.com>
2024-02-12 15:48:15 +0700

    ☀︎ sidebars

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -2,5 +2,15 @@ import { defineConfig } from 'vocs'
  
 export default defineConfig({
   description: 'Smarter Accounts',
-  title: 'NANI'
+  title: 'NANI',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/docs/',
+    },
+    {
+      text: 'Contracts',
+      link: '/docs/contracts'
+    }
+  ]
 })
