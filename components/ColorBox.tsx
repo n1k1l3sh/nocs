cb1c7f64734dfeb38a2b9aef89dce3e4cad81225
Shivanshi Tyagi <shivanshityagi@icloud.com>
2024-11-27 08:55:17 +0700

    update

--- components/ColorBox.tsx
+++ components/ColorBox.tsx
@@ -1,37 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
-export function ColorBox({ color, name }) {
+const Toast = ({ message, visible, onClose }) => {
+  if (!visible) return null;
+
   return (
     <div
       style={{
-        backgroundColor: color,
-        width: "100px",
-        height: "100px",
-        display: "inline-block",
-        margin: "10px",
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        backgroundColor: "#333",
+        color: "white",
+        padding: "12px 24px",
         borderRadius: "4px",
-        position: "relative",
-        border: "1px solid #ccc",
-        boxShadow: "0 0 0 1px #fff, 0 0 0 2px #ccc",
+        animation: "fadeIn 0.3s, fadeOut 0.3s 2.7s",
+        zIndex: 1000,
       }}
     >
+      {message}
+    </div>
+  );
+};
+
+export function ColorBox({ color, name }) {
+  const [showToast, setShowToast] = useState(false);
+
+  // Helper function to determine if a color is dark
+  const isColorDark = (color) => {
+    const hex = color.replace("#", "");
+    const r = parseInt(hex.substr(0, 2), 16);
+    const g = parseInt(hex.substr(2, 2), 16);
+    const b = parseInt(hex.substr(4, 2), 16);
+    return r * 0.299 + g * 0.587 + b * 0.114 < 128;
+  };
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(color);
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 3000);
+  };
+
+  return (
+    <>
       <div
+        onClick={handleClick}
         style={{
-          padding: "8px",
-          color: color === "white" || color === "black" ? "#666" : "black",
-          fontSize: "12px",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          overflow: "hidden",
-          textOverflow: "ellipsis",
+          backgroundColor: color,
+          width: "100px",
+          height: "100px",
+          display: "inline-block",
+          margin: "10px",
+          borderRadius: "4px",
+          position: "relative",
+          border: "1px solid #ccc",
+          boxShadow: "0 0 0 1px #fff, 0 0 0 2px #ccc",
+          cursor: "pointer",
         }}
       >
-        {name}
-        <br />
-        {color}
+        <div
+          style={{
+            padding: "8px",
+            color:
+              color === "white"
+                ? "#666"
+                : isColorDark(color)
+                  ? "white"
+                  : "black",
+            fontSize: "12px",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {name}
+          <br />
+          {color}
+        </div>
       </div>
-    </div>
+      <Toast
+        message="Color code copied to clipboard!"
+        visible={showToast}
+        onClose={() => setShowToast(false)}
+      />
+    </>
   );
 }
--- vocs.config.tsx
+++ vocs.config.tsx
@@ -3,7 +3,38 @@ import { defineConfig } from "vocs";
 export default defineConfig({
   rootDir: "./",
   description: "Your onchain agent",
+  banner: {
+    dismissable: true,
+    backgroundColor: "cyan",
+    content: "Head to our new [Discord](https://discord.gg/PFHZa7KPPc)!",
+    height: "38px",
+    textColor: "white",
+  },
   title: "NANI",
+  baseUrl: "https://docs.nani.ooo",
+  blogDir: "./pages/blog",
+  editLink: {
+    pattern: "https://github.com/NaniDAO/nocs/edit/main/:path",
+    text: "Edit on GitHub",
+  },
+  iconUrl: "/favicon.svg",
+  logoUrl: "/logo.svg",
+  markdown: {
+    code: {
+      themes: {
+        light: "vitesse-light",
+        dark: "vitesse-dark",
+      },
+    },
+  },
+  font: {
+    default: {
+      google: "Inter",
+    },
+    mono: {
+      google: "JetBrains Mono",
+    },
+  },
   sidebar: [
     {
       text: "Getting Started",
@@ -14,8 +45,54 @@ export default defineConfig({
       link: "/docs/contracts",
     },
     {
-      text: "Brand",
-      link: "/brand",
+      text: "Account",
+      link: "/docs/account",
+    },
+    {
+      text: "Accounts",
+      link: "/docs/accounts",
+    },
+    {
+      text: "Create Account",
+      link: "/docs/create-account",
+    },
+    {
+      text: "DAO",
+      link: "/docs/dao",
+    },
+    {
+      text: "IE",
+      link: "/docs/ie",
+    },
+    {
+      text: "NEETH",
+      link: "/docs/neeth",
+    },
+    {
+      text: "Text Commands",
+      link: "/docs/text-commands",
+    },
+  ],
+  socials: [
+    {
+      icon: "discord",
+      link: "https://discord.gg/PFHZa7KPPc",
+    },
+    {
+      icon: "github",
+      link: "https://github.com/NaniDAO",
+    },
+    {
+      icon: "telegram",
+      link: "https://t.me/+NKbETPq0J9UyODk9",
+    },
+    {
+      icon: "x",
+      link: "https://x.com/nani__ooo",
+    },
+    {
+      icon: "warpcast",
+      link: "https://warpcast.com/nani",
     },
   ],
 });
